e44a5b0e1a48ec6c5f3cd357c3555760cb4edb6e
Oluwatobi Akanji <tobi@542.digital>
2024-08-07 11:43:27 +0100

    chore: testing config out

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -2,6 +2,4 @@ import { DigitalOceanConfig } from './digitalocean.config';
 
 console.log({ IS_PROD: process.env.IS_PROD, dd: process.env.MYSQLHOST });
 const IS_PROD = JSON.parse(process.env.IS_PROD);
-export const EnvConfig = IS_PROD
-  ? { ...process.env, ...DigitalOceanConfig }
-  : process.env;
+export const EnvConfig = IS_PROD ? DigitalOceanConfig : process.env;
