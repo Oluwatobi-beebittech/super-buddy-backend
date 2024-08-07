6480f5379521ce0de729514f3604e4fe1c5fe9a7
Oluwatobi Akanji <tobi@542.digital>
2024-08-07 13:14:17 +0100

    fix: database config

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,5 +1,4 @@
 import { DigitalOceanConfig } from './digitalocean.config';
 
-console.log({ IS_PROD: process.env.IS_PROD, dd: process.env.MYSQLHOST });
 const IS_PROD = JSON.parse(process.env.IS_PROD);
 export const EnvConfig = IS_PROD ? DigitalOceanConfig : process.env;
